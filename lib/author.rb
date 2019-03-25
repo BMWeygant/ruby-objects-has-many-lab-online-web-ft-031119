ca3059801bce63753ee05381b66ce29ef22e4b22
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-25 16:02:50 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,19 @@
+class Author
+attr_accessor :name
+
+
+def initialize(name)
+@name = name
+@posts = []
+end
+
+def add_post(post)
+  self.posts 
+post.author = self
+end
+
+def posts
+  @posts
+end
+
+end
